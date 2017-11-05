30bf3552f7f28fdd9d3cb2ceffd6a9d90c7c610b
mattschreiber <matt.schreiber01@gmail.com>
2017-11-05 09:55:14 -0500

    added logic to print the balance total

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
   "budget/models"
+  "fmt"
+  "time"
 )
 
 func main() {
   models.InitDB()
-  models.AllBudgetEntries()
+
+  budgetEntries, _ := models.AllBudgetEntries()
+  for _, v := range budgetEntries {
+    fmt.Printf("%d, %d, %d, %d, %d, %t, %s\n", v.Id, v.Credit, v.Debit, v.Store_id, v.Category_id, v.Applied, v.Trans_date.Format(time.RFC822))
+  }
+
+  budgetTotal, err := models.BudgetTotal()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Printf("Balance: %d\n", budgetTotal)
 }
