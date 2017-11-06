1c9dd0bde27c7178807b8b7f5d7baa15a93cd6e6
mattschreiber <matt.schreiber01@gmail.com>
2017-11-06 08:34:16 -0500

    test code for printing new functions ProjectedBalance and BudgetTotal

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+  "encoding/json"
   "budget/models"
   "fmt"
   "time"
+  "os"
 )
 
 func main() {
@@ -13,11 +15,24 @@ func main() {
   for _, v := range budgetEntries {
     fmt.Printf("%d, %d, %d, %d, %d, %t, %s\n", v.Id, v.Credit, v.Debit, v.Store_id, v.Category_id, v.Applied, v.Trans_date.Format(time.RFC822))
   }
-
-  budgetTotal, err := models.BudgetTotal()
+  budgetTotal, err := models.BudgetTotal(time.Now())
   if err != nil {
     fmt.Println(err)
     return
   }
   fmt.Printf("Balance: %d\n", budgetTotal)
+
+  projBalance, err := models.ProjectedBalance()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  enc := json.NewEncoder(os.Stdout)
+  d := map[string]int{"projBalance": projBalance}
+  enc.Encode(d)
+
+  enc1 := json.NewEncoder(os.Stdout)
+  budgetEntry, _ := models.BudgetEntry(1)
+  enc1.Encode(budgetEntry)
+  // fmt.Println(budgetEntry)
 }
