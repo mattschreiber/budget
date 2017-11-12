096608f45d10e436fcb4698cdfef2853b0df1513
mattschreiber <matt.schreiber01@gmail.com>
2017-11-12 10:25:15 -0500

    Category Model with function AllCategories to return a list of all categories

---
+++ models/category.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+  "fmt"
+)
+
+type Category struct {
+  Id int `json:"id"`
+  Category_name string `json:"category_name"`
+}
+
+func AllCategories() ([]Category, error) {
+  rows, err := db.Query("SELECT c.id, c.category_name FROM category as c order by c.category_name")
+  if err != nil {
+    fmt.Println(err)
+    return nil, err
+  }
+  defer rows.Close()
+
+  var categories []Category
+  for rows.Next() {
+    var category Category
+    err := rows.Scan(&category.Id, &category.Category_name)
+    if err != nil {
+      return nil, err
+    }
+    categories = append(categories, category)
+  }
+  if err = rows.Err(); err != nil {
+   return nil, err
+  }
+
+  return categories, nil
+
+}
