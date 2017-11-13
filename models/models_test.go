20efd2dd6e14e8d646b27628bf5ef9a53c6a7389
mattschreiber <matt.schreiber01@gmail.com>
2017-11-13 15:25:33 -0500

    added tests for currentPayPeriod method

--- models/models_test.go
+++ models/models_test.go
@@ -3,7 +3,6 @@ package models
 import (
   "testing"
   "time"
-  "fmt"
 )
 
 func TestPrevDate(t *testing.T) {
@@ -19,9 +18,27 @@ func TestPrevDate(t *testing.T) {
 
   for _, test := range tests {
     resultDate := prevPayDate(test.startDate)
-    fmt.Println("test")
     if resultDate != test.prevDate {
       t.Errorf("Incorrect date %v", resultDate  )
+    }
+  }
+}
+
+func TestCurrentPayPeriod(t *testing.T) {
+  type testDate struct {
+    startDate time.Time
+    prevDate time.Time
+  }
+  tests := []testDate{ {time.Date(2017, 11, 14, 0, 0, 0, 0, time.UTC), time.Date(2017, 11, 15, 0, 0, 0, 0, time.UTC)},
+    {time.Date(2017, 11, 16, 0, 0, 0, 0, time.UTC), time.Date(2017, 12, 1, 0, 0, 0, 0, time.UTC)},
+    {time.Date(2017, 12, 31, 0, 0, 0, 0, time.UTC), time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)},
+    {time.Date(2017, 12, 14, 0, 0, 0, 0, time.UTC), time.Date(2017, 12, 15, 0, 0, 0, 0, time.UTC)},
+  }
+
+  for _, test := range tests {
+    resultDate := currentPayPeriod(test.startDate)
+    if resultDate != test.prevDate {
+      t.Errorf("Incorrect Pay period date %v", resultDate  )
     }
   }
 }
