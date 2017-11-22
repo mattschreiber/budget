6039cb20115119970621bbc96d529a460bb7d39b
mattschreiber <matt.schreiber01@gmail.com>
2017-11-22 09:40:56 -0500

    added routes

---
+++ controllers/budgets_controller.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+  "encoding/json"
+  "budget/models"
+  "fmt"
+  "time"
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+const layout = "2006-1-2"
+
+type TokenContextKey struct {
+  Name string
+}
+
+func GetProjBalance(w http.ResponseWriter, req *http.Request) {
+
+  t := TokenContextKey{}
+  if token := req.Context().Value(t.Name); token != nil {
+		// User is logged in
+	} else {
+		// User is not logged in
+	}
+  params := mux.Vars(req)
+  // layout := "2006-1-2"
+  // startDate, _ := time.Parse(layout, "1900-01-01")
+  endDate, _ := time.Parse(layout, params["endDate"])
+
+  projBalance, err := models.ProjectedBalance(endDate)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  // enc := json.NewEncoder(os.Stdout)
+  d := map[string]int{"projBalance": projBalance}
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(d)
+
+}
+
+func GetBalanceEntries(w http.ResponseWriter, req *http.Request) {
+  params := mux.Vars(req)
+  startDate, _ := time.Parse(layout, params["startDate"])
+  endDate, _ := time.Parse(layout, params["endDate"])
+  budgetEntries, _ := models.AllBudgetEntries(startDate, endDate)
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(budgetEntries)
+}
