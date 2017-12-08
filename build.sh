ac5090c94cad6743c0cd0f200dbc464fd2a929cb
mattschreiber <matt.schreiber01@gmail.com>
2017-12-07 20:22:00 -0500

    attempt to deploy to aws

---
+++ build.sh
@@ -0,0 +1,6 @@
+go get github.com/gorilla/mux
+go get github.com/dgrijalva/jwt-go
+go get github.com/dgrijalva/jwt-go/request
+go get github.com/lib/pq
+go get golang.org/x/crypto/bcrypt
+go build -o bin/application application.go
--- models/db.go
+++ models/db.go
@@ -2,29 +2,29 @@ package models
 
 import (
   "fmt"
-  // "os"
+  "os"
   "database/sql"
   _ "github.com/lib/pq"
 )
 
-const (
-  host = "localhost"
-  port = 5432
-  user = "postgres"
-  dbname = "budget_development"
-)
+// const (
+//   host = "localhost"
+//   port = 5432
+//   user = "postgres"
+//   dbname = "budget_development"
+// )
 var db *sql.DB
 func InitDB() {
 
-  // host := "mattdb.clf0wvbr9kqy.us-east-2.rds.amazonaws.com"
-  // port := 5432
-  // user := os.Getenv("AWS_DB_USER")
-  // password := os.Getenv("AWS_MYBUDGET_DB_PW")
-  // dbname := "budget_development"
+  host := "mattdb.clf0wvbr9kqy.us-east-2.rds.amazonaws.com"
+  port := 5432
+  user := os.Getenv("AWS_DB_USER")
+  password := os.Getenv("AWS_MYBUDGET_DB_PW")
+  dbname := "budget_development"
 
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "dbname=%s sslmode=disable",
-    host, port, user, dbname)
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s "+
+    "dbname=%s sslmode=require",
+    host, port, user, password, dbname)
 
   var err error
   db, err = sql.Open("postgres", psqlInfo)
