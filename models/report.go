2d7555d1e8c27ab966497885a249c8d5c65d214d
mattschreiber <matt.schreiber01@gmail.com>
2018-01-01 09:08:24 -0500

    changed AmountsByCategory to return flat array of category amounts

--- models/report.go
+++ models/report.go
@@ -13,27 +13,27 @@ type CategoryAmounts struct {
 
 // This function returns a map with keys for ledger and budget. Each key contains
 // an array of CategoryAmounts, which are calculated totals for a user provided time period.
-func AmountsByCategory(startDate, endDate time.Time) (map[string][]CategoryAmounts, error) {
+func AmountsByCategory(startDate, endDate time.Time) ([]CategoryAmounts, error) {
   rows, err := db.Query(`SELECT
     l_cat,
     ABS(sum(actual)) as actual,
     ABS(sum(budget)) as budget
-FROM
-(
-    SELECT
-      category_id, c.category_name as l_cat, (credit-debit) as actual , 0 as budget
-    FROM
-      ledger join category c on ledger.category_id = c.id
-    where trans_date between '2017-12-1' AND '2017-12-31'
-    UNION ALL
-    SELECT
-      category_id, c.category_name as l_cat, 0 as actual, (credit-debit) as budget
-    FROM
-      budget join category c on budget.category_id = c.id
-    where trans_date between $1 AND $2
-) x
-GROUP BY
-    category_id, l_cat`, startDate, endDate)
+  FROM
+  (
+      SELECT
+        category_id, c.category_name as l_cat, (credit-debit) as actual , 0 as budget
+      FROM
+        ledger join category c on ledger.category_id = c.id
+      where trans_date between '2017-12-1' AND '2017-12-31'
+      UNION ALL
+      SELECT
+        category_id, c.category_name as l_cat, 0 as actual, (credit-debit) as budget
+      FROM
+        budget join category c on budget.category_id = c.id
+      where trans_date between $1 AND $2
+  ) x
+  GROUP BY
+      category_id, l_cat`, startDate, endDate)
 
   if err != nil {
     fmt.Println(err)
@@ -58,17 +58,17 @@ GROUP BY
    return nil, err
   }
 
-  var m map[string][]CategoryAmounts
-  m = make(map[string][]CategoryAmounts)
-
-  for _, ca := range arrCa {
-    if ca.Budget > 0 {
-      m["budget"] = append(m["budget"], ca)
-    }
-    if ca.Ledger > 0 {
-      m["ledger"] = append(m["ledger"], ca)
-    }
-  }
-  return m, nil
+  // var m map[string][]CategoryAmounts
+  // m = make(map[string][]CategoryAmounts)
+  //
+  // for _, ca := range arrCa {
+  //   if ca.Budget > 0 {
+  //     m["budget"] = append(m["budget"], ca)
+  //   }
+  //   if ca.Ledger > 0 {
+  //     m["ledger"] = append(m["ledger"], ca)
+  //   }
+  // }
+  return arrCa, nil
 
 }
