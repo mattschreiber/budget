56783ca16108564f7730d3d31d2156e6e2a7f0a8
mattschreiber <matt.schreiber01@gmail.com>
2018-01-12 10:12:23 -0500

    package for sending email

---
+++ email/email.go
@@ -0,0 +1,105 @@
+package email
+
+import (
+  "fmt"
+  "crypto/tls"
+  "log"
+  "net/smtp"
+  "strings"
+)
+
+type Mail struct {
+	senderId string
+	toIds    []string
+	subject  string
+	body     string
+}
+
+type SmtpServer struct {
+	host string
+	port string
+}
+
+
+func (s *SmtpServer) ServerName() string {
+	return s.host + ":" + s.port
+}
+
+func (mail *Mail) BuildMessage() string {
+	message := ""
+	message += fmt.Sprintf("From: %s\r\n", mail.senderId)
+	if len(mail.toIds) > 0 {
+		message += fmt.Sprintf("To: %s\r\n", strings.Join(mail.toIds, ";"))
+	}
+
+	message += fmt.Sprintf("Subject: %s\r\n", mail.subject)
+	message += "\r\n" + mail.body
+
+	return message
+}
+
+func SendMail(body string) {
+
+  mail := Mail{}
+	mail.senderId = "matt.schreiber01@gmail.com"
+	mail.toIds = []string{"matt.schreiber01@gmail.com"}
+	mail.subject = "New Ledger Entries"
+	mail.body = body
+
+	messageBody := mail.BuildMessage()
+
+	smtpServer := SmtpServer{host: "smtp.gmail.com", port: "465"}
+
+	//build an auth
+	auth := smtp.PlainAuth("", mail.senderId, "ozxydsyxozpzihzj", smtpServer.host)
+
+	// Gmail will reject connection if it's not secure
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         smtpServer.host,
+	}
+
+	conn, err := tls.Dial("tcp", smtpServer.ServerName(), tlsconfig)
+	if err != nil {
+		log.Println(err)
+	}
+
+	client, err := smtp.NewClient(conn, smtpServer.host)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// step 1: Use Auth
+	if err = client.Auth(auth); err != nil {
+		log.Println(err)
+	}
+
+	// step 2: add all from and to
+	if err = client.Mail(mail.senderId); err != nil {
+		log.Println(err)
+	}
+	for _, k := range mail.toIds {
+		if err = client.Rcpt(k); err != nil {
+			log.Println(err)
+		}
+	}
+
+	// Data
+	w, err := client.Data()
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = w.Write([]byte(messageBody))
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Println(err)
+	}
+
+	client.Quit()
+}
