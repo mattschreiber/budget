ec2d74347719dad315b7f46b2168ad2584b76567
mattschreiber <matt.schreiber01@gmail.com>
2018-01-12 10:12:41 -0500

    package for scheduling tasks

---
+++ tasks/scheduled_jobs.go
@@ -0,0 +1,36 @@
+package tasks
+
+import (
+  // "fmt"
+  "time"
+  "budget/models"
+)
+
+func ScheduledTasks() {
+
+  t := time.Now()
+  n := time.Date(t.Year(), t.Month(), t.Day(), 3, 0, 0, 0, t.Location())
+
+  if t.After(n)  {
+    // run job immediately and then wait until 3am tomorrow
+    models.AutoPay()
+    n = n.Add(24 * time.Hour)
+  }
+
+  d := n.Sub(t)
+  time.AfterFunc(d, tick)
+
+
+}
+
+func tick() {
+  // start ticker that will run once per day
+  ticker := time.NewTicker(time.Hour * 24)
+  // run Auto pay then start goroutine for ticker and continue to run autopay once per every 24 hours
+  models.AutoPay()
+    go func() {
+        for _ = range ticker.C {
+          models.AutoPay()
+        }
+    }()
+}
