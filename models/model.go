c4e780c498c11251b57b1ee8c344e6466aa1ca8c
mattschreiber <matt.schreiber01@gmail.com>
2018-04-03 11:42:13 -0400

    deleted old code that is no longer needed

--- models/model.go
+++ models/model.go
@@ -37,94 +37,6 @@ func getEst() (*time.Location) {
     }
     return utc
 }
-// This is a function that will return both the ledger and budget totals for a given time period
-// func GetAmountSpent(startDate time.Time, endDate time.Time) (total TotalAmounts, err error) {
-//   budget_closed, ledger_closed := false, false
-//   c1 := make(chan Balance) // channel for Balance amount and error handling
-//   c2 := make(chan Balance) // channel for Balance amount and error handling
-//
-//   go GetBudgetBalance(startDate, endDate, c1)
-//   go GetLedgerBalance(startDate, endDate, c2)
-//
-//   for {
-//     if (budget_closed && ledger_closed) {
-//       return total, nil
-//     }
-//     select {
-//     case budgetBal := <-c1:
-//       if budgetBal.Error != nil {
-//         total.BudgetAmount = 0
-//       }else {
-//         total.BudgetAmount = budgetBal.Amount
-//       }
-//       budget_closed = true
-//     case ledgerBal := <-c2:
-//       if ledgerBal.Error != nil {
-//         total.LedgerAmount = 0
-//       }else {
-//         total.LedgerAmount = ledgerBal.Amount
-//       }
-//         ledger_closed = true
-//     }
-//   }
-// }
-
-// ProjectedBalance is a function that calculates the total budgeted balance at any given time.
-// The function accepts an ending date and returns the balance in cents as an integer
-// The projected balance is calculated by first finding the current actual ledger balance up through the end of the
-// previous pay period which wil be the 14th or last day of month
-// The second step is to calculate the budgeted balance starting with the current pay period and up until a user provided end Date
-// The default is calulated using the currentPayPeriod and the end of the next pay period
-// The last step is to sum the ledger balance with the budget balance.
-// Example: today is 2017-12-15.
-// Ledger query between beginningOfTime and 2017-12-14
-// Budget query between 2017-12-15 and 2017-12-31
-// func ProjectedBalance(endDate time.Time) (projBalance int, err error) {
-//   c := make(chan Balance) // channel for Balance amount and error handling
-//
-//   pd := time.Now().In(getEst())
-//   pd = time.Date(pd.Year(), pd.Month(), 14, 0, 0, 0, 0, getEst()) // ledger range should end on 14th of this month
-//   firstOfMonth := time.Date(pd.Year(), pd.Month(), 1, 0, 0, 0, 0, getEst())
-//   lastOfPrevMonth := firstOfMonth.AddDate(0, 0, -1) // ledger range should be until end of last month
-//   lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
-//   // firstOfNextMonth := firstOfMonth.AddDate(0, 1, 0)
-//
-//   // If the end date provided is after the current pay period then find all budget entries starting with the
-//   // the beginning of the next pay period and ending on the user provided end date
-//   if endDate.After(currentPayPeriod(time.Now().In(getEst()))) || endDate.Equal(currentPayPeriod(time.Now().In(getEst()))) {
-//     go GetBudgetBalance(currentPayPeriod(time.Now().In(getEst())), endDate, c)
-//   }else {
-//     if currentPayPeriod(time.Now().In(getEst())).Day() == 1 {
-//       go GetBudgetBalance(prevPayDate(time.Now().In(getEst())), lastOfMonth, c)
-//     } else {
-//       go GetBudgetBalance(prevPayDate(time.Now().In(getEst())), pd, c)
-//     }
-//   }
-//   // If the end date for the projection time period is before the current pay period ends then
-//   // only include ledger entries through the previous pay period. Otherwise include all entries
-//   // until the provided end date. The first parament is just a dummy date used to get the entire ledger balance.
-//   if endDate.Before(currentPayPeriod(time.Now().In(getEst()))) {
-//     if currentPayPeriod(time.Now().In(getEst())).Day() == 1 {
-//       go GetLedgerBalance(beginningOfTime(), pd, c)
-//     } else {
-//       go GetLedgerBalance(beginningOfTime(), lastOfPrevMonth, c)
-//     }
-//   }else {
-//     go GetLedgerBalance(beginningOfTime(), currentPayPeriod(time.Now().In(getEst())).AddDate(0,0,-1), c)
-//   }
-//
-//   budgetBal, ledgerBal := <-c, <-c // receive Balance struct from go routines
-//   //
-//   if budgetBal.Error != nil || ledgerBal.Error != nil {
-//     if budgetBal.Error != nil {
-//       return -1, budgetBal.Error
-//     }else {
-//       return -1, ledgerBal.Error
-//     }
-//   }
-//
-//   return ledgerBal.Amount + budgetBal.Amount, nil
-// }
 
 // function that returns an object consisting of all categories and all stores
 func GetStoreCategory() (StoreCat, error){
@@ -243,3 +155,93 @@ func ProjectedBalance(endDate time.Time) (projBalance int, err error) {
   wg.Wait()
   return total.LedgerAmount + total.BudgetAmount, nil
 }
+
+
+// This is a function that will return both the ledger and budget totals for a given time period
+// func GetAmountSpent(startDate time.Time, endDate time.Time) (total TotalAmounts, err error) {
+//   budget_closed, ledger_closed := false, false
+//   c1 := make(chan Balance) // channel for Balance amount and error handling
+//   c2 := make(chan Balance) // channel for Balance amount and error handling
+//
+//   go GetBudgetBalance(startDate, endDate, c1)
+//   go GetLedgerBalance(startDate, endDate, c2)
+//
+//   for {
+//     if (budget_closed && ledger_closed) {
+//       return total, nil
+//     }
+//     select {
+//     case budgetBal := <-c1:
+//       if budgetBal.Error != nil {
+//         total.BudgetAmount = 0
+//       }else {
+//         total.BudgetAmount = budgetBal.Amount
+//       }
+//       budget_closed = true
+//     case ledgerBal := <-c2:
+//       if ledgerBal.Error != nil {
+//         total.LedgerAmount = 0
+//       }else {
+//         total.LedgerAmount = ledgerBal.Amount
+//       }
+//         ledger_closed = true
+//     }
+//   }
+// }
+
+// ProjectedBalance is a function that calculates the total budgeted balance at any given time.
+// The function accepts an ending date and returns the balance in cents as an integer
+// The projected balance is calculated by first finding the current actual ledger balance up through the end of the
+// previous pay period which wil be the 14th or last day of month
+// The second step is to calculate the budgeted balance starting with the current pay period and up until a user provided end Date
+// The default is calulated using the currentPayPeriod and the end of the next pay period
+// The last step is to sum the ledger balance with the budget balance.
+// Example: today is 2017-12-15.
+// Ledger query between beginningOfTime and 2017-12-14
+// Budget query between 2017-12-15 and 2017-12-31
+// func ProjectedBalance(endDate time.Time) (projBalance int, err error) {
+//   c := make(chan Balance) // channel for Balance amount and error handling
+//
+//   pd := time.Now().In(getEst())
+//   pd = time.Date(pd.Year(), pd.Month(), 14, 0, 0, 0, 0, getEst()) // ledger range should end on 14th of this month
+//   firstOfMonth := time.Date(pd.Year(), pd.Month(), 1, 0, 0, 0, 0, getEst())
+//   lastOfPrevMonth := firstOfMonth.AddDate(0, 0, -1) // ledger range should be until end of last month
+//   lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
+//   // firstOfNextMonth := firstOfMonth.AddDate(0, 1, 0)
+//
+//   // If the end date provided is after the current pay period then find all budget entries starting with the
+//   // the beginning of the next pay period and ending on the user provided end date
+//   if endDate.After(currentPayPeriod(time.Now().In(getEst()))) || endDate.Equal(currentPayPeriod(time.Now().In(getEst()))) {
+//     go GetBudgetBalance(currentPayPeriod(time.Now().In(getEst())), endDate, c)
+//   }else {
+//     if currentPayPeriod(time.Now().In(getEst())).Day() == 1 {
+//       go GetBudgetBalance(prevPayDate(time.Now().In(getEst())), lastOfMonth, c)
+//     } else {
+//       go GetBudgetBalance(prevPayDate(time.Now().In(getEst())), pd, c)
+//     }
+//   }
+//   // If the end date for the projection time period is before the current pay period ends then
+//   // only include ledger entries through the previous pay period. Otherwise include all entries
+//   // until the provided end date. The first parament is just a dummy date used to get the entire ledger balance.
+//   if endDate.Before(currentPayPeriod(time.Now().In(getEst()))) {
+//     if currentPayPeriod(time.Now().In(getEst())).Day() == 1 {
+//       go GetLedgerBalance(beginningOfTime(), pd, c)
+//     } else {
+//       go GetLedgerBalance(beginningOfTime(), lastOfPrevMonth, c)
+//     }
+//   }else {
+//     go GetLedgerBalance(beginningOfTime(), currentPayPeriod(time.Now().In(getEst())).AddDate(0,0,-1), c)
+//   }
+//
+//   budgetBal, ledgerBal := <-c, <-c // receive Balance struct from go routines
+//   //
+//   if budgetBal.Error != nil || ledgerBal.Error != nil {
+//     if budgetBal.Error != nil {
+//       return -1, budgetBal.Error
+//     }else {
+//       return -1, ledgerBal.Error
+//     }
+//   }
+//
+//   return ledgerBal.Amount + budgetBal.Amount, nil
+// }
