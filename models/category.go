e60d20264d9751ce83a473759407d29636080280
mattschreiber <matt.schreiber01@gmail.com>
2020-12-22 12:55:30 -0500

    added comments

--- models/category.go
+++ models/category.go
@@ -1,36 +1,36 @@
 package models
 
 import (
-  "fmt"
+	"fmt"
 )
 
 type Category struct {
-  Id int `json:"id"`
-  Category_name string `json:"category_name"`
+	Id            int    `json:"id"`
+	Category_name string `json:"category_name"`
 }
 
 // A function that returns a slice containing every category fromt
 func GetAllCategories() ([]Category, error) {
-  rows, err := db.Query("SELECT c.id, c.category_name FROM category as c order by c.category_name")
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
-  defer rows.Close()
+	rows, err := db.Query("SELECT c.id, c.category_name FROM category as c order by c.category_name")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
 
-  var categories []Category
-  for rows.Next() {
-    var category Category
-    err := rows.Scan(&category.Id, &category.Category_name)
-    if err != nil {
-      return nil, err
-    }
-    categories = append(categories, category)
-  }
-  if err = rows.Err(); err != nil {
-   return nil, err
-  }
+	var categories []Category
+	for rows.Next() {
+		var category Category
+		err := rows.Scan(&category.Id, &category.Category_name)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
 
-  return categories, nil
+	return categories, nil
 
 }
