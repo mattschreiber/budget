5dbb119e7afd958809e9e0944472fb32a61f8a2e
mattschreiber <matt.schreiber01@gmail.com>
2020-12-22 12:52:32 -0500

    added comments

--- models/auth.go
+++ models/auth.go
@@ -1,23 +1,25 @@
 package models
 
 import (
-  // "budget/middlewares"
-  "strings"
-  "fmt"
+	// "budget/middlewares"
+	"fmt"
+	"strings"
 )
 
+// UserCredentials ...
 type UserCredentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
+// VerifyLogin ...
 func VerifyLogin(user UserCredentials) (string, error) {
-  var email string
-  var passwordHash string
-  err := db.QueryRow("SELECT email, password FROM users WHERE email = $1", strings.ToLower(user.Username)).Scan(&email, &passwordHash)
-  if err != nil {
-    fmt.Println(err)
-    return "", err
-  }
-  return passwordHash, nil
+	var email string
+	var passwordHash string
+	err := db.QueryRow("SELECT email, password FROM users WHERE email = $1", strings.ToLower(user.Username)).Scan(&email, &passwordHash)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return passwordHash, nil
 }
